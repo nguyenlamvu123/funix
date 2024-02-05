38f2ed11080465db13fd7f72ab2192081003f19d
zaibachkhoa <nguyenlamvu123@gmail.com>
2024-02-05 16:09:39 +0700

    done lab 9.7

---
+++ FUNiX--PRO192x.3.1.VN/Lab 9.7.java
@@ -0,0 +1,53 @@
+class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(int name) {
+        this.salary = name;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+
+class Manager extends Employee {
+    private int bonus;
+
+    public Manager(String name, int salary, int bonus) {
+        super(name, salary);
+        this.bonus = bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getSalary() {
+        return super.salary + this.bonus;
+    }
+
+    public void display() {
+        System.out.println("Name: " + getName());
+        System.out.println("Salary: " + getSalary());
+    }
+}
