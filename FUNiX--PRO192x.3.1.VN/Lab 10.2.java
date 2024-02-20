800aa26e602b7f3790d8ca94465bcc78baa6681b
zaibachkhoa <nguyenlamvu123@gmail.com>
2024-02-20 11:19:41 +0700

    - cần code thêm lab 10.2

---
+++ FUNiX--PRO192x.3.1.VN/Lab 10.2.java
@@ -0,0 +1,43 @@
+class Printer {
+	private int tonerLevel;
+	private double duplex;
+	
+	void Printer(int tonerLevel, double duplex) {
+		if (tonerLevel >= 0 && tonerLevel <= 100) {
+			this.tonerLevel = tonerLevel;
+		} else {
+			this.tonerLevel = -1;
+		}
+		this.duplex = duplex;
+	}
+	
+	int addToner(int luongmucdovao) {
+		if (luongmucdovao >= 0 && luongmucdovao <= 100) {
+			int tonerLevel_ = this.tonerLevel + luongmucdovao;
+			if (tonerLevel_ < 100) {
+				this.tonerLevel = tonerLevel_;
+			} else {
+				tonerLevel_ = -1;
+			}
+		} else {
+			tonerLevel_ = -1;
+		}
+		return tonerLevel_;
+	}
+	
+	int printPages(int pagesPrinted) {
+		int sotocanin
+		if (this.duplex) {
+			sotocanin = pagesPrinted / 2;
+		} else {
+			sotocanin = pagesPrinted;
+		}
+		return sotocanin;
+	}
+	
+	void getPagePrinted(int sotocanin) {
+		for (int pp = 0, pp < sotocanin, pp++) {
+			System.out.println("Đang in tờ số" + pp);
+		}
+	}
+}
