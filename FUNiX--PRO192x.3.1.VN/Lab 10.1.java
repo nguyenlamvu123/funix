9504297ed088bf0795b8f876a255ed76fdfe5bdb
zaibachkhoa <nguyenlamvu123@gmail.com>
2024-02-29 16:42:35 +0700

    - Lab 10.1 đã chạy được

--- FUNiX--PRO192x.3.1.VN/Lab 10.1.java
+++ FUNiX--PRO192x.3.1.VN/Lab 10.1.java
@@ -1,9 +1,31 @@
+class main {
+	public static void main(String[] args) {
+	   // Bedroom challenge
+		Wall wall1 = new Wall("West");
+		Wall wall2 = new Wall("East");
+		Wall wall3 = new Wall("South");
+		Wall wall4 = new Wall("North");
+
+		Ceiling ceiling = new Ceiling(12, 55);
+
+		Bed bed = new Bed("Modern", 4, 3, 2,1);
+
+		Lamp lamp = new Lamp("Classic", false, 75);
+
+		Bedroom bedroom = new Bedroom("Alberts", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+		bedroom.makeBed();
+
+		bedroom.getLamp().turnOn();
+	}
+}
+
+
 class Lamp {
 	private String style;
 	private boolean battery;
 	private int globRating;
 	
-	void Lamp(String style, boolean battery, int globRating) {
+	Lamp(String style, boolean battery, int globRating) {
 		this.style = style;
 		this.battery = battery;
 		this.globRating = globRating;
@@ -38,13 +60,14 @@ class Bed {
 	private int sheets;
 	private int quilt;
 	
-	void Bed (String style, int pillows, int height, int sheets, int quilt) {
+	Bed (String style, int pillows, int height, int sheets, int quilt) {
 		this.pillows = pillows;
 		this.style = style;
 		this.height = height;
 		this.sheets = sheets;
 		this.quilt = quilt;
 	}
+	
 	
 	void make() {  // không có kiểu trả về và in ra một thông báo về hiệu ứng rằng chiếc giường đang được tạo ra; 
 		System.out.println("chiếc giường đang được tạo ra");
@@ -81,7 +104,7 @@ class Ceiling {
 	private int height;
 	private int paintedColor;
 	
-	void Ceiling (int height, int paintedColor) {
+	Ceiling (int height, int paintedColor) {
 		this.height = height;
 		this.paintedColor = paintedColor;
 	}
@@ -101,7 +124,7 @@ class Ceiling {
 class Wall {
 	private String direction;
 	
-	void Wall (int height, int paintedColor) {
+	Wall (String direction) {
 		this.direction = direction;
 	}
 	
@@ -115,12 +138,12 @@ class Wall {
 class Bedroom {
 	private String name; 
 	private Wall wall1, wall2, wall3, wall4;
-	private Celling ceiling; 
+	private Ceiling ceiling; 
 	private Bed bed;
 	private Lamp lamp;
 
 	
-	void Bedroom (String style, int pillows, int height, int sheets, int quilt) {
+	Bedroom (String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
 		this.wall1 = wall1;
 		this.wall2 = wall2;
 		this.wall3 = wall3;
@@ -138,24 +161,5 @@ class Bedroom {
 	void makeBed() {  // in thông báo rằng Bed đang được tạo ra và cũng gọi phương thức make () trong lớp Bed
 		bed.make();
 	}
-}
-
-
-public static void main(String[] args) {  // Bedroom challenge
-	Wall wall1 = new Wall("West");
-	Wall wall2 = new Wall("East");
-	Wall wall3 = new Wall("South");
-	Wall wall4 = new Wall("North");
-
-	Ceiling ceiling = new Ceiling(12, 55);
-
-	Bed bed = new Bed("Modern", 4, 3, 2,1);
-
-	Lamp lamp = new Lamp("Classic", false, 75);
-
-	Bedroom bedroom = new Bedroom("Alberts", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
-	bedroom.makeBed();
-
-	bedroom.getLamp().turnOn();
 }
 
