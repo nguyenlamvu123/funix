87cda25746f54e40db1fd0bfb9a7ba2f72188b29
zaibachkhoa <nguyenlamvu123@gmail.com>
2024-03-01 17:29:55 +0700

    done lab 10.6

--- FUNiX--PRO192x.3.1.VN/Lab 10.4.java
+++ FUNiX--PRO192x.3.1.VN/Lab 10.4.java
@@ -1,3 +1,16 @@
+public class Entry {
+	public static void main(String[] args) {
+		Rectangle r = new Rectangle(4, 5);
+		System.out.println("Area: " + r.getArea());
+		System.out.println("Perimeter: " + r.getPerimeter());
+		r.setLength(2);
+		r.setWidth(3);
+		System.out.println("Area: " + r.getArea());
+		System.out.println("Perimeter: " + r.getPerimeter());
+	}
+}
+
+
 class Rectangle {
     private double length;
     private double width;
